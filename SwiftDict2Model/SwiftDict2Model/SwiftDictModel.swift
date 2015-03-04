053ec8881457e233988f42890e61eac0b7295c27
大任 <zrtalent@gmail.com>
2015-03-04 12:25:00 +0800

    使用运行时知识获取模型中的属性和类型

--- SwiftDict2Model/SwiftDict2Model/SwiftDictModel.swift
+++ SwiftDict2Model/SwiftDict2Model/SwiftDictModel.swift
@@ -11,7 +11,23 @@ import Foundation
 class SwiftDictModel {
     
     func modelInfo(cls: AnyClass) {
-        println(cls)
+        
+        // 运行时知识
+        var count: UInt32 = 0
+        let ivars = class_copyIvarList(cls, &count)
+        // 遍历ivars数组，
+        for i in 0..<count {
+            let ivar = ivars[Int(i)]
+            // UInt8 = char,获取变量名
+            let cname = ivar_getName(ivar)
+            let name = String.fromCString(cname)
+            // 获取变量类型
+            let ctype = ivar_getTypeEncoding(ivar)
+            let type = String.fromCString(ctype)
+            println("->\(name)-->\(type)") // 应为Swift的bug所以type取不出来
+        }
+        
+        
     }
     
     
